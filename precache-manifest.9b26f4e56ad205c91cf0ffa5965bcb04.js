60563d19f3a42fc73f41c1863bf2b5860ced66df
Anton Kablukov <antoha7@gmail.com>
2020-03-22 18:10:07 +0100

    Updates

---
+++ precache-manifest.9b26f4e56ad205c91cf0ffa5965bcb04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd8ae312e9b9ade594bea1ab6031835",
+    "url": "/react-form/index.html"
+  },
+  {
+    "revision": "da91f178a00a3265fbda",
+    "url": "/react-form/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "9f51420aeb9b544cf1ad",
+    "url": "/react-form/static/css/main.1620057f.chunk.css"
+  },
+  {
+    "revision": "da91f178a00a3265fbda",
+    "url": "/react-form/static/js/2.d457e537.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/react-form/static/js/2.d457e537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f51420aeb9b544cf1ad",
+    "url": "/react-form/static/js/main.5ea10543.chunk.js"
+  },
+  {
+    "revision": "f6becc9cea3ac08cc9ac",
+    "url": "/react-form/static/js/runtime-main.22a3593e.js"
+  },
+  {
+    "revision": "59337baed905d6196632ae1986fb2086",
+    "url": "/react-form/static/media/default-avatar.59337bae.png"
+  }
+]);
